62f8965a445a4d86463a96b0130ad9d5919aaead
malhotra1432 <malhotra1432@gmail.com>
2021-09-28 19:58:07 +0530

    [PM] adds googe code format plugin

--- src/main/java/com/kodo/exercise/ExerciseApplication.java
+++ src/main/java/com/kodo/exercise/ExerciseApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExerciseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExerciseApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ExerciseApplication.class, args);
+  }
 }
